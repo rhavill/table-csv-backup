a292fa9a99c8271f8ef91f41ca79f66f8f8aec50
Ricardo Havill <rhavill2002@yahoo.com>
2016-07-15 22:51:24 -0400

    Added download buttons.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,26 +2,44 @@
 <html>
 <head>
     <title>CSV Backup</title>
-    <meta charset="utf-8" />
+    <meta charset="utf-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+          integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
+          integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <style type="text/css">
+        .table-name {
+            overflow: hidden;
+        }
+    </style>
 </head>
 <body>
 <div class="container">
     <div class="page-header"><h1>hello</h1></div>
     @if (count($tables) > 0)
         <ul class="list-group">
-        @foreach ($tables as $table)
-            <li class="list-group-item">
-                {{ $table }}
-            </li>
-        @endforeach
+            @foreach ($tables as $table)
+                <li class="list-group-item">
+                    <div class="row">
+                        <div class="col-xs-8 table-name">
+                            {{ $table }}
+                        </div>
+                        <div class="col-xs-4 text-right">
+                            <a class="btn btn-default btn-xs" href="#" role="button">
+                                <span class="glyphicon glyphicon-download" aria-hidden="true">
+                                </span> Download
+                            </a>
+                        </div>
+                    </div>
+                </li>
+            @endforeach
         </ul>
     @endif
     {{--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>--}}
