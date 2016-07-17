6c874914d91546021fbcbfab3a7248eae7f63297
Ricardo Havill <rhavill2002@yahoo.com>
2016-07-17 17:26:34 -0400

    Added a bootstrap CSS theme.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,8 +7,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
         integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
-        integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+  {{--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"--}}
+        {{--integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">--}}
+  <link rel="stylesheet" href="http://bootswatch.com/slate/bootstrap.min.css" />
   <link href="/ricardo/fine-uploader/fine-uploader.css" rel="stylesheet" type="text/css"/>
   <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
   <!--[if lt IE 9]>
@@ -47,7 +48,7 @@
               {{ $table }}
             </div>
             <div class="col-xs-4 text-right">
-              <a class="btn btn-default btn-xs"
+              <a class="btn btn-default btn-xs btn-primary"
                  href="{{ url('table/'.$table) }}"
                  role="button">
                 <span class="glyphicon glyphicon-download" aria-hidden="true">
